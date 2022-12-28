52c5bd37545cf1ecd1677053b07c668628627150
CHUCHUDAN <daniel870217@gmail.com>
2022-12-28 14:37:01 +0800

    first version

---
+++ card.js
@@ -0,0 +1,175 @@
+//狀態機
+const GAME_STATE = {
+  FirstCardAwaits: "FirstCardAwaits",
+  SecondCardAwaits: "SecondCardAwaits",
+  CardsMatchFailed: "CardsMatchFailed",
+  CardsMatched: "CardsMatched",
+  GameFinished: "GameFinished",
+}
+
+
+//花色資料
+const Symbols = [
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png', // 黑桃
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png', // 愛心
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png', // 方塊
+  'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
+]
+
+
+//前端呈現function
+const view = {
+
+  //生成每張卡背面HTML
+  getCardElement(index) {
+    return `
+      <div class="card back" data-index='${index}'></div>`
+  },
+
+  //生成每張卡正面HTML
+  getCardContent(index) {
+    const number = this.transformNumber((index % 13) + 1)
+    const symbol = Symbols[Math.floor(index / 13)]
+    return `
+    <p>${number}</p>
+    <img src="${symbol}" alt="error">
+    <p>${number}</p>
+    `
+  },
+
+  //呈現卡片在HTML
+  displayCards(indexes) {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = indexes.map(index => this.getCardElement(index)).join('')
+  },
+  //AJQK轉換
+  transformNumber(number) {
+    switch (number) {
+      case 1: return 'A'
+      case 11: return 'J'
+      case 12: return 'Q'
+      case 13: return 'K'
+      default: return number
+    }
+  },
+  filpCards(...cards) {
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index))
+        return
+      }
+      card.classList.add('back')
+      card.innerHTML = null
+    })
+  },
+  pairCards(...cards) {
+    cards.map(card => card.classList.add('paired'))
+  },
+  renderScore(score) {
+    document.querySelector('.score').textContent = `Score: ${score}`
+  },
+  renderTriedTimes(times) {
+    document.querySelector('.tried').textContent = `You've tried: ${times} times`
+  },
+  appendWrongAnimation(...cards) {
+    cards.map(card => {
+      card.classList.add('wrong')
+      card.addEventListener('animationend', (event) => {
+        event.target.classList.remove('wrong'), { once: true }
+      })
+    })
+  },
+  showGameFinished() {
+    const div = document.createElement('div')
+    div.classList.add('completed')
+    div.innerHTML = `
+      <p>Complete!</p>
+      <p>Score: ${model.score}</p>
+      <p>You've tried: ${model.triedTimes} times</p>
+    `
+    const header = document.querySelector('#header')
+    header.before(div)
+  }
+}
+
+//流程管理function
+const controller = {
+  currentState: GAME_STATE.FirstCardAwaits,
+  generateCards() {
+    view.displayCards(utility.getRandomNumberArray(52))
+  },
+  dispatchCardAction(card) {
+    if (!card.classList.contains('back')) return
+    switch (this.currentState) {
+      case GAME_STATE.FirstCardAwaits:
+        view.filpCards(card)
+        model.revealedCards.push(card)
+        this.currentState = GAME_STATE.SecondCardAwaits
+        break
+      case GAME_STATE.SecondCardAwaits:
+        view.renderTriedTimes(++model.triedTimes)
+        view.filpCards(card)
+        model.revealedCards.push(card)
+        if (model.isRevealedCardsMatched()) {
+          this.currentState = GAME_STATE.CardsMatched
+          view.renderScore(model.score += 10)
+          view.pairCards(...model.revealedCards)
+          model.revealedCards = []
+          if (model.score === 260) {
+            console.log('showGameFinished')
+            this.currentState = GAME_STATE.GameFinished
+            view.showGameFinished()  // 加在這裡
+            return
+          }
+          this.currentState = GAME_STATE.FirstCardAwaits
+        } else {
+          this.currentState = GAME_STATE.CardsMatchFailed
+          view.appendWrongAnimation(...model.revealedCards)
+          setTimeout((this.resetCards), 1000);
+        }
+        break
+    }
+    console.log('currentState: ', this.currentState)
+    console.log('cardList: ', model.revealedCards.map(card => card.dataset.index))
+
+  },
+  resetCards() {
+    view.filpCards(...model.revealedCards)
+    model.revealedCards = []
+    controller.currentState = GAME_STATE.FirstCardAwaits
+  }
+}
+
+//資料管理function
+const model = {
+  revealedCards: [],
+  isRevealedCardsMatched() {
+    return this.revealedCards[0].dataset.index % 13 === this.revealedCards[1].dataset.index % 13
+  },
+  score: 0,
+  triedTimes: 0
+}
+
+//外掛function
+const utility = {
+  //將牌組陣列洗牌
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1))
+        ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
+    }
+    return number
+  }
+}
+
+controller.generateCards()
+
+//以下為監聽器
+
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    controller.dispatchCardAction(card)
+  })
+})
